893de0c2cbec8bc643ffa02ac2d8e7db1a44af78
Susanne Heim <git@s-heim.eu>
2024-06-23 20:26:29 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,14 +8,17 @@ public class Main {
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         try {
-        serverSocket = new ServerSocket(4221);
-        // Since the tester restarts your program quite often, setting SO_REUSEADDR
-        // ensures that we don't run into 'Address already in use' errors
-        serverSocket.setReuseAddress(true);
-        clientSocket = serverSocket.accept(); // Wait for connection from client.
-        System.out.println("accepted new connection");
+            serverSocket = new ServerSocket(4221);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            clientSocket = serverSocket.accept(); // Wait for connection from client.
+            System.out.println("accepted new connection");
+            OutputStream output = clientSocket.getOutputStream();
+            String message = "HTTP/1.1 200 OK\r\n\r\n";
+            output.write(message.getBytes());
         } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
+            System.out.println("IOException: " + e.getMessage());
         }
     }
 }
